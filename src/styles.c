46fb0f24c5216baef3822381c46175b06a7bdcef
Belousov Oleg <belousov.oleg@gmail.com>
2022-12-28 03:41:49 +0300

    Fonts

--- src/styles.c
+++ src/styles.c
@@ -15,6 +15,11 @@ lv_style_t  freq_main_style;
 lv_style_t  waterfall_style;
 lv_style_t  btn_style;
 
+extern lv_font_t    eco_sans_14;
+extern lv_font_t    eco_sans_16;
+extern lv_font_t    eco_sans_32;
+extern lv_font_t    eco_sans_36;
+
 static const uint16_t radius = 25;
 
 void styles_init() {
@@ -32,12 +37,13 @@ void styles_init() {
 
     lv_style_init(&freq_style);
     lv_style_set_text_color(&freq_style, lv_color_white());
-    lv_style_set_text_font(&freq_style, &lv_font_montserrat_16);
+    lv_style_set_text_font(&freq_style, &eco_sans_16);
     lv_style_set_pad_ver(&freq_style, 10);
 
     lv_style_init(&freq_main_style);
     lv_style_set_text_color(&freq_main_style, lv_color_white());
-    lv_style_set_text_font(&freq_main_style, &lv_font_montserrat_32);
+    lv_style_set_text_font(&freq_main_style, &eco_sans_36);
+    lv_style_set_pad_ver(&freq_main_style, 1);
 
     lv_style_init(&waterfall_style);
     lv_style_set_bg_color(&waterfall_style, lv_color_hex(0x000000));
@@ -55,6 +61,6 @@ void styles_init() {
     lv_style_set_border_opa(&btn_style, LV_OPA_50);
     lv_style_set_border_color(&btn_style, lv_color_hex(0x000000));
     lv_style_set_border_width(&btn_style, 2);
-    lv_style_set_text_font(&btn_style, &lv_font_montserrat_16);
+    lv_style_set_text_font(&btn_style, &eco_sans_16);
     lv_style_set_radius(&btn_style, radius);
 }
