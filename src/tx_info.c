9e9e171e0652746095abd2b7d4e360406321fb6c
Belousov Oleg <belousov.oleg@gmail.com>
2023-01-09 01:55:26 +0300

    Small improvements TX info

--- src/tx_info.c
+++ src/tx_info.c
@@ -5,24 +5,26 @@
  *
  *  Copyright (c) 2022-2023 Belousov Oleg aka R1CBU
  */
+ 
+#include <stdio.h>
 
 #include "tx_info.h"
 #include "styles.h"
 #include "events.h"
 
-static uint8_t      over = 25;
-static uint8_t      height = 54 * 2;
+static uint8_t          over = 25;
+static uint8_t          height = 54 * 2;
 
-static float        pwr = 10.0f;
-static float        vswr = 5.0f;
-static float        alc;
+static float            pwr = 10.0f;
+static float            vswr = 5.0f;
+static float            alc;
 
-static lv_obj_t     *obj;
+static lv_obj_t         *obj;
+static lv_grad_dsc_t    grad;
 
 static void tx_info_draw_cb(lv_event_t * e) {
     lv_obj_t            *obj = lv_event_get_target(e);
     lv_draw_ctx_t       *draw_ctx = lv_event_get_draw_ctx(e);
-    lv_draw_line_dsc_t  line_dsc;
     lv_draw_rect_dsc_t  rect_dsc;
     lv_draw_label_dsc_t label_dsc;
     lv_area_t           area;
@@ -46,19 +48,19 @@ static void tx_info_draw_cb(lv_event_t * e) {
     area.x1 = x1;
     area.y1 = y1;
     area.x2 = x1 + 50;
-    area.y2 = y1 + 30;
+    area.y2 = y1 + 32;
 
     lv_draw_label(draw_ctx, &label_dsc, &area, "SWR", NULL);
 
     label_dsc.font = &eco_sans_18;
 
     for (int i = 1; i <= 5; i++) {
-        area.x1 = x1 + 60 + len * i / 5.0f;
+        area.x1 = x1 + 60 + len * i / 5;
         area.y1 = y1 + 15 - 9;
         area.x2 = area.x1 + 20;
         area.y2 = area.y1 + 16;
 
-        snprintf(buf, sizeof(buf), "%i", i);
+        snprintf(buf, sizeof(buf), i == 5 ? ">%i" : "%i", i);
         lv_draw_label(draw_ctx, &label_dsc, &area, buf, NULL);
     }
 
@@ -69,14 +71,14 @@ static void tx_info_draw_cb(lv_event_t * e) {
     area.x1 = x1;
     area.y1 = y1 + 54;
     area.x2 = x1 + 50;
-    area.y2 = y1 + 30 + 54;
+    area.y2 = y1 + 32 + 54;
     
     lv_draw_label(draw_ctx, &label_dsc, &area, "PWR", NULL);
 
     label_dsc.font = &eco_sans_18;
 
     for (int i = 2; i <= 10; i += 2) {
-        area.x1 = x1 + 60 + len * i / 10.0f;
+        area.x1 = x1 + 60 + len * i / 10;
         area.y1 = y1 + 54 + 15 - 9;
         area.x2 = area.x1 + 20;
         area.y2 = area.y1 + 16;
@@ -89,17 +91,17 @@ static void tx_info_draw_cb(lv_event_t * e) {
 
     lv_draw_rect_dsc_init(&rect_dsc);
 
-    rect_dsc.bg_color = lv_color_white();
+    rect_dsc.bg_grad = grad;
     rect_dsc.bg_opa = LV_OPA_50;
     rect_dsc.radius = 10;
     rect_dsc.border_width = 2;
-    rect_dsc.border_color = lv_color_white();
+    rect_dsc.border_color = lv_color_black();
     rect_dsc.border_opa = LV_OPA_50;
 
     area.x1 = x1 + 70;
     area.y1 = y1;
     area.x2 = area.x1 + len * vswr / 5.0f;
-    area.y2 = y1 + 30;
+    area.y2 = y1 + 32;
 
     lv_draw_rect(draw_ctx, &rect_dsc, &area);
 
@@ -135,6 +137,19 @@ lv_obj_t * tx_info_init(lv_obj_t *parent) {
     lv_obj_add_event_cb(obj, tx_cb, EVENT_RADIO_TX, NULL);
     lv_obj_add_event_cb(obj, rx_cb, EVENT_RADIO_RX, NULL);
     lv_obj_add_event_cb(obj, tx_info_draw_cb, LV_EVENT_DRAW_MAIN_END, NULL);
+
+    grad.dir = LV_GRAD_DIR_VER;
+    grad.stops_count = 4;
+
+    grad.stops[0].color = lv_color_lighten(bg_color, 200);
+    grad.stops[1].color = bg_color;
+    grad.stops[2].color = bg_color;
+    grad.stops[3].color = lv_color_darken(bg_color, 200);
+    
+    grad.stops[0].frac  = 0;
+    grad.stops[1].frac  = 128 - 10;
+    grad.stops[2].frac  = 128 + 10;
+    grad.stops[3].frac  = 255;
    
     return obj;
 }
