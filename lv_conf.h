320bd309fbb8d72805746852bb25d9fea4fbeb6a
Belousov Oleg <belousov.oleg@gmail.com>
2023-01-11 18:55:31 +0300

    Reduced the polling rate of input devices for stability

--- lv_conf.h
+++ lv_conf.h
@@ -86,7 +86,7 @@ extern uint32_t custom_tick_get(void);
 #define LV_DISP_DEF_REFR_PERIOD (1000/50)      /*[ms]*/
 
 /*Input device read period in milliseconds*/
-#define LV_INDEV_DEF_READ_PERIOD (1000/50)     /*[ms]*/
+#define LV_INDEV_DEF_READ_PERIOD (1000/20)     /*[ms]*/
 
 /*Use a custom tick source that tells the elapsed time in milliseconds.
  *It removes the need to manually update the tick with `lv_tick_inc()`)*/
